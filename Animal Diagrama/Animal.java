ea1b4f29f4a8d61fd5855cb254fdb27a7987a430
Laura <laura.escobarca@campusucc.edu.co>
2026-05-29 14:35:23 -0500

    refactor

--- Animal Diagrama/Animal.java
+++ Animal Diagrama/Animal.java
@@ -6,8 +6,16 @@ public class Animal {
         this.name = name;
     }
 
+    protected Color color;
+
+    public Animal(String name, Color color) {
+        this.name = name;
+        this.color = color
+    }
+
     @Override
     public String toString() {
-        return "Animal: nombre = \"" + name + "\".";
+
+        return "Animal: nombre = \"" + name + ", color= " + color;
     }
 }
